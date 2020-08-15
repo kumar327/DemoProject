3b4ab5b7aaca2c5f6f67fce1a89509f479d3dedb
Kumar <adigoppulakumar@gmail.com>
2020-08-15 18:01:41 +0530

    Project Stracture is created

---
+++ GitDemoProject/src/com/aks/bean/Registration.java
@@ -0,0 +1,22 @@
+package com.aks.bean;
+
+public class Registration {
+
+	private int eno;
+	private String ename;
+	
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+}
---
+++ GitDemoProject/src/com/aks/test/TestDemo.java
@@ -0,0 +1,8 @@
+package com.aks.test;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Test Demo is created");
+	}
+}
